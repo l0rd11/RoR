f1912f4cb59587353e030c234e0dbb60aba6f60d
l0rd11 <majarosz.j50@gmail.com>
2016-06-15 15:16:03 +0200

    cosmetick fixes

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,41 +1,37 @@
 class FacebookController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_loggin?
+  before_action :check_loggin? , :init
   respond_to :html
+
+  def init
+    @access_token = session["facebook_access_token"]
+    @graph = Koala::Facebook::API.new(@access_token)
+  end
 
   def new
 
   end
 
   def destroy
-    @access_token = session["facebook_access_token"]
-    @graph = Koala::Facebook::API.new(@access_token)
+
     @graph.delete_object(params[:id])
     redirect_to "/facebook", :notice => "Post deleted."
   end
 
   def create
     if params[:post]
-      @access_token = session["facebook_access_token"]
-      @graph = Koala::Facebook::API.new(@access_token)
       @graph.put_wall_post(params[:post])
     end
     redirect_to "/facebook", :notice => "Post created."
   end
 
   def index
-    @access_token = session["facebook_access_token"]
-    @graph = Koala::Facebook::API.new(@access_token)
-
     @posts = params[:page] ? @graph.get_page(params[:page]) :@graph.get_connections('me', 'posts')
     @posts.reject!{|p| p["message"].nil?||p["message"].empty?}
     respond_with(@posts)
   end
 
   def show
-    @access_token = session["facebook_access_token"]
-    @graph = Koala::Facebook::API.new(@access_token)
-
     @posts = params[:page] ? @graph.get_page(params[:page]) :@graph.get_connections('me', 'posts')
     @posts.reject!{|p| p["message"].nil?||p["message"].empty?}
     respond_with(@posts)
